f75712247ec593530a42d11769257af48da0500c
大ædamingerdai <mingguobin@live.com>
2020-08-12 22:27:01 +0800

    add v1 api

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -1,6 +1,7 @@
 package routers
 
 import (
+	v1 "github.com/damingerdai/blog-service/internal/routers/api/v1"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,11 +10,27 @@ func NewRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 
-	api := r.Group("/api/v1")
+	article := v1.NewArticle()
+	tag := v1.NewTag()
+	apiV1 := r.Group("/api/v1")
 	{
-		api.Any("/ping", func(c *gin.Context) {
+		apiV1.Any("/ping", func(c *gin.Context) {
 			c.JSON(200, gin.H{"message": "pong"})
 		})
+
+		apiV1.POST("/tag", tag.Create)
+		apiV1.DELETE("/tag/:id", tag.Delete)
+		apiV1.PUT("/tag/:id", tag.Update)
+		apiV1.PATCH("/tag/:id/state", tag.Update)
+		apiV1.GET("/tag/:id", tag.Get)
+		apiV1.GET("/tags", tag.List)
+
+		apiV1.POST("/article", article.Create)
+		apiV1.DELETE("/article/:id", article.Delete)
+		apiV1.PUT("/article/:id", article.Update)
+		apiV1.PATCH("/article/:id/state", article.Update)
+		apiV1.GET("/article/:id", article.Get)
+		apiV1.GET("/articles", article.List)
 	}
 
 	return r
