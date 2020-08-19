5215aca64b22e57de3ba28f7cc4a91b46118f028
大ædamingerdai <mingguobin@live.com>
2020-08-19 22:21:27 +0800

    use viper to fetch setting

---
+++ global/setting.go
@@ -0,0 +1,11 @@
+package global
+
+import (
+	"github.com/damingerdai/blog-service/pkg/setting"
+)
+
+var (
+	ServerSetting   *setting.ServerSettingS
+	AppSetting      *setting.AppSettingS
+	DatabaseSetting *setting.DatabaseSettings
+)
