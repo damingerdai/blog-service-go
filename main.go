5c51ce9c7497752d72cadf19f6ed341fe6a77552
大ædamingerdai <mingguobin@live.com>
2020-08-19 22:32:54 +0800

    read config when initing the application

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"github.com/damingerdai/blog-service/global"
+
 	"github.com/damingerdai/blog-service/internal/routers"
+	"github.com/damingerdai/blog-service/pkg/setting"
 )
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init setup Setting err: %v", err)
+	}
+}
 
 func main() {
 	// r := gin.Default()
@@ -21,4 +32,29 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 	s.ListenAndServe()
+}
+
+func setupSetting() error {
+	setting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+
+	err = setting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+
+	return nil
 }
